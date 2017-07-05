3f7787a24e049eeefa0b16facfc359c5f2ee017d
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-07-05 16:17:43 -0500

    skills bars working
    skills bars working

--- src/components/NavIcons.js
+++ src/components/NavIcons.js
@@ -7,9 +7,9 @@ class NavIcons extends Component {
       <Row>
         <Col id="iconContainer" xs={10} xsOffset={1}>
           <a href="#aboutMe"><img alt="aboutMe" className="icon grow" src={require('./gfx/aboutMe.png')} /></a>
+          <a href="#skills"><img alt="skills" className="icon grow" src={require('./gfx/skills.png')} /></a>
           <a href="#resume"><img alt="resume" className="icon grow" src={require('./gfx/resume.png')} /></a>
           <a href="#network"><img alt="network" className="icon grow" src={require('./gfx/network.png')} /></a>
-          <a href="#skills"><img alt="skills" className="icon grow" src={require('./gfx/skills.png')} /></a>
         </Col>
       </Row>
     );
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 class Skills extends Component {
+
+
   render() {
     return (
-      <div >
-        Skills
-      </div>
+
+        <Row>
+          <Col xs={8} xsOffset={2}>
+            <a name="skills"></a>
+            <h2 id="skillsH2">SKILLS</h2>
+              <ul id="skill">
+                <li><span className="bar html-css"></span><h3>HTML/CSS</h3></li>
+                <li><span className="bar javascript"></span><h3>JavaScript</h3></li>
+                <li><span className="bar bootstrap"></span><h3>Bootstrap</h3></li>
+                <li><span className="bar jquery"></span><h3>jQuery</h3></li>
+                <li><span className="bar angular"></span><h3>AngularJS</h3></li>
+                <li><span className="bar react"></span><h3>React</h3></li>
+              </ul>
+          </Col>
+        </Row>
+
     );
   }
 }
