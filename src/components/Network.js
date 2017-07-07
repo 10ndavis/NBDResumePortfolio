b617f4657cc4e5109b7084684c495234dc7daea8
Nathan Brewer-Davis <10ndavis10@gmail.com>
2017-07-07 15:38:38 -0500

    minor styling changes
    minor styling changes

--- src/components/Network.js
+++ src/components/Network.js
@@ -7,9 +7,8 @@ class Network extends Component {
       <Row id="network">
         <Col xs={12} md={8} mdOffset={2}>
           <Col xs={12}>
-            <h2>Connect with me:</h2>
+            <h2 id="connect">Connect with me:</h2>
           </Col>
-          <br/>
           <Row>
             <Col xs={4} xsOffset={2} md={3} mdOffset={3}>
               <a href="https://linkedin.com/in/10ndavis"><img alt="LinkedIn" className="icon grow" src={require('./gfx/linkedin.png')} /></a>
